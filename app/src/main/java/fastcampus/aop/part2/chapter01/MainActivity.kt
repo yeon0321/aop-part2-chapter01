03f6e8dcde560d46ea52d795a80c83f01c901977
yeonji <yeon0321@swu.ac.kr>
2022-06-01 16:04:00 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
@@ -0,0 +1,13 @@
+package fastcampus.aop.part2.chapter01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
